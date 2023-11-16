f89cc1ff017af892893aeca927f3f3224a092e1e
sabrlui <sabrlui@gmail.com>
2023-11-16 11:27:26 +0000

    función navigateToScreen Ajuste de estilo

--- screens/DistrictScreen.js
+++ screens/DistrictScreen.js
@@ -1,30 +1,29 @@
-import { View, SafeAreaView, ScrollView, StyleSheet,TouchableOpacity, Text } from 'react-native'
-import React from 'react'
-import DistrictList from '../components/DistrictList'
-import DateScreen from './DateScreen'
+import React from 'react';
+import { View, SafeAreaView, ScrollView, StyleSheet, TouchableOpacity, Text } from 'react-native';
+import DistrictList from '../components/DistrictList';
 
 export default function DistrictScreen({ navigation }) {
+  const navigateToScreen = (screenName) => {
+    navigation.navigate(screenName);
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.menuButton}>
-        <TouchableOpacity onPress={() => navigation.navigate(
-              'DistrictScreen'
-          )} style={styles.appButtonContainer}>
-          <Text style={styles.appButtonText}>{"Distritos"}</Text>
+        <TouchableOpacity onPress={() => navigateToScreen('DistrictScreen')} style={styles.appButtonContainer}>
+          <Text style={styles.appButtonText}>Distritos</Text>
         </TouchableOpacity>
-        <TouchableOpacity onPress={() => navigation.navigate(
-              'DateScreen'
-          )} style={styles.appButtonContainer}>
-          <Text style={styles.appButtonText}>{"Calendario"}</Text>
+        <TouchableOpacity onPress={() => navigateToScreen('DateScreen')} style={styles.appButtonContainer}>
+          <Text style={styles.appButtonText}>Calendario</Text>
         </TouchableOpacity>
       </View>
       <SafeAreaView style={styles.container}>
         <ScrollView>
-          <DistrictList navigation={navigation} style={styles.DistrictList}/>
+          <DistrictList navigation={navigation} style={styles.districtList} />
         </ScrollView>
       </SafeAreaView>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
@@ -34,11 +33,10 @@ const styles = StyleSheet.create({
     borderRadius: 1,
     paddingVertical: 10,
     paddingHorizontal: 12,
-    marginBottom:4,
-    flex:1,
-    borderColor:"#1B263B",
-    borderWidth : 1
-
+    marginBottom: 4,
+    flex: 1,
+    borderColor: "#1B263B",
+    borderWidth: 1,
   },
   appButtonText: {
     fontSize: 14,
@@ -47,12 +45,16 @@ const styles = StyleSheet.create({
     alignSelf: "center",
     textTransform: "uppercase"
   },
-  container:{
+  container: {
     backgroundColor: "#1B263B",
-    flex:1
+    flex: 1,
   },
   menuButton: {
     flexDirection: 'row',
     alignItems: 'center',
+    justifyContent: 'space-between', // Añadido para separar los botones
+    paddingHorizontal: 20, // Añadido para espacio en los lados
+    paddingVertical: 10, // Añadido para espacio arriba y abajo
   },
+
 });
